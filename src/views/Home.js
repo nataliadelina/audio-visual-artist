501707dfd252cae799de9d0544704d30c1e2b5c7
Natalia <natalia_alexandra@web.de>
2022-04-11 11:08:40 +0200

    add font, home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import heroImg from '../assets/images/hero-img.png'
 import playIcon from '../assets/icons/play-icon.png'
+import mainWork1 from '../assets/images/main-img-1.png'
+import mainWork2 from '../assets/images/main-img-2.png'
+import mainWork3 from '../assets/images/main-img-3.png'
 
 export default function Home() {
     return (
         <div>
-            <div className="hero-desc">
-                <h2>JuanSessions</h2>
-                <h1>Video & Audio </h1>
-                <h2>Content Production</h2>
-                <p>Creative and active person, ready to achieve
-                    more experience and work hard in Video and
-                    Audio production</p>
-                <Link to="/work" className="btn btn-primary">explore</Link>
-                <Link to="/contact" className="btn btn-secondary">contact</Link>
+            <div className="hero-desc" id='home'>
+                <h1>Audiovisual Artist </h1>
+                {/* <h2>Content Production</h2> */}
+                <p>Video Editing, Sound & Soundtracks Composition, Visual Effects, Animation </p>
+                {/* <Link to="/work" className="btn btn-primary">explore</Link>
+                <Link to="/contact" className="btn btn-secondary">contact</Link> */}
             </div>
             <div className="hero-img">
                 <img src={playIcon} alt="dragon" />
                 <img src={heroImg} alt="dragon" />
             </div>
+
+            <div className="main-work" id='main-work'>
+                <img src={mainWork1} alt="work" />
+                <img src={mainWork2} alt="work" />
+                <img src={mainWork3} alt="work" />
+            </div>
+
+            <div className="main-audio" id='main-audio'>
+
+            </div>
+
+            <div className='parallax' id="main-bg-img"></div>
         </div>
     )
 }
