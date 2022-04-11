f1e79fbe6b5cffdcade8388db14d04479eb5505b
Natalia <natalia_alexandra@web.de>
2022-04-11 10:05:37 +0200

    video

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,7 +1,13 @@
 import React from 'react'
+// import video from '../assets/videos/dragonComplet.mp4'
 
 export default function Work() {
     return (
-        <div>Work</div>
+        <div>
+            <h2>work</h2>
+            {/* <video width="750" height="500" controls >
+                <source src={video} type="video/mp4" />
+            </video> */}
+        </div>
     )
 }
