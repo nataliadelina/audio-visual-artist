b26a86c0df7bed19b9b99ac8d6275bf7bda1c34c
Natalia <natalia_alexandra@web.de>
2022-04-11 12:27:21 +0200

    home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,56 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import heroImg from '../assets/images/hero-img.png'
-import playIcon from '../assets/icons/play-icon.png'
+import playMainIcon from '../assets/icons/play-icon.png'
+import playIcon from '../assets/icons/play.png'
 import mainWork1 from '../assets/images/main-img-1.png'
 import mainWork2 from '../assets/images/main-img-2.png'
 import mainWork3 from '../assets/images/main-img-3.png'
 import Card3D from '../components/Card3D'
+import '../style/home.css'
 
 export default function Home() {
     return (
         <div>
-            <div className="hero-desc" id='home'>
-                <h1>Audiovisual Artist </h1>
-                {/* <h2>Content Production</h2> */}
-                <p>Video Editing, Sound & Soundtracks Composition, Visual Effects, Animation </p>
-                {/* <Link to="/work" className="btn btn-primary">explore</Link>
+            <div className="main flex-column-center">
+                <div className="main-desc " id='home'>
+                    <h1>Audiovisual Artist </h1>
+                    {/* <h2>Content Production</h2> */}
+                    <p>Video Editing, Sound & Soundtracks Composition, Visual Effects, Animation </p>
+                    {/* <Link to="/work" className="btn btn-primary">explore</Link>
                 <Link to="/contact" className="btn btn-secondary">contact</Link> */}
-            </div>
-            <div className="hero-img">
-                <img src={playIcon} alt="dragon" />
-                <img src={heroImg} alt="dragon" />
+                </div>
+
+                <div className="main-img">
+                    <a href="">
+                        <img src={playMainIcon} alt="dragon" className='playMainIcon' />
+                    </a>
+                    <img src={heroImg} alt="dragon" />
+                </div>
             </div>
 
-            <div className="main-work" id='main-work'>
-                <img src={mainWork1} alt="work" />
-                <img src={mainWork2} alt="work" />
-                <img src={mainWork3} alt="work" />
+            <div className="main-work flex-row-center" id='main-work'>
+                <div className="main-work-video flex-column">
+                    <img src={mainWork1} alt="work" className='main-work-video-img' />
+                    <div className='main-work-desc flex-row'>
+                        <h3>biking</h3>
+                        <img src={playIcon} alt="dragon" className='playIcon' />
+                    </div>
+                </div>
+                <div className="main-work-video">
+                    <img src={mainWork2} alt="work" className='main-work-video-img' />
+                    <div className='main-work-desc flex-row'>
+                        <h3>flow organically</h3>
+                        <img src={playIcon} alt="dragon" className='playIcon' />
+                    </div>
+                </div>
+                <div className="main-work-video">
+                    <img src={mainWork3} alt="work" className='main-work-video-img' />
+                    <div className='main-work-desc flex-row'>
+                        <h3>often alone</h3>
+                        <img src={playIcon} alt="dragon" className='playIcon' />
+                    </div>
+                </div>
             </div>
 
             <Card3D />
@@ -34,7 +59,7 @@ export default function Home() {
 
             </div>
 
-            <div className='parallax' id="main-bg-img"></div>
+            <div className='parallax juan-studio-img' id="main-bg-img"></div>
         </div>
     )
 }
